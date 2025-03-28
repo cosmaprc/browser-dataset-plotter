5b333bf4668142812f49836db367abac3b9d6c85
Cosmin Purcherea <cosmin.purcherea@gmail.com>
2025-03-28 21:58:40 +0000

    Fixes

--- home.py
+++ home.py
@@ -1,38 +1,38 @@
 import datetime
 import os
 
-import pandas
-import plotly
-import streamlit
+import pandas as pd
+import plotly.express as px
+import streamlit as st
 
 import utils
 
 
 def main() -> None:
-    streamlit.set_page_config(
+    st.set_page_config(
         page_title="Pandas Viewer", layout="wide", page_icon="🐼"
     )
-    streamlit.subheader("Pandas Plot Viewer", divider=True)
-    streamlit.success(
+    st.subheader("Pandas Plot Viewer", divider=True)
+    st.success(
         "Welcome to the Pandas Viewer! Upload a CSV file to get started."
     )
-    streamlit.caption(
+    st.caption(
         "This is a simple Streamlit app that allows you to upload a \
             CSV file and view the data in a Pandas DataFrame."
     )
-    streamlit.subheader("File Upload", divider=True)
-    with streamlit.form("Upload a CSV data file", clear_on_submit=True):
-        file = streamlit.file_uploader(
+    st.subheader("File Upload", divider=True)
+    with st.form("Upload a CSV data file", clear_on_submit=True):
+        file = st.file_uploader(
             "Upload a CSV data file", type=["csv"], accept_multiple_files=False
         )
-        if streamlit.form_submit_button("Upload File"):
+        if st.form_submit_button("Upload File"):
             if not file:
-                streamlit.warning("Please select a file to upload.")
+                st.warning("Please select a file to upload.")
             else:
                 utils.save_file(file)
 
-    streamlit.subheader("File Information", divider=True)
-    file_info_df = pandas.DataFrame({"File": os.listdir(utils.get_data_dir())})
+    st.subheader("File Information", divider=True)
+    file_info_df = pd.DataFrame({"File": os.listdir(utils.get_data_dir())})
     file_info_df["Creation Time"] = file_info_df["File"].apply(
         lambda x: datetime.datetime.fromtimestamp(
             os.path.getctime(os.path.join(utils.get_data_dir(), x))
@@ -48,61 +48,43 @@ def main() -> None:
     )
 
     file_info_df.sort_values("Creation Time", ascending=False, inplace=True)
-    streamlit.dataframe(file_info_df, use_container_width=True)
+    st.dataframe(file_info_df, use_container_width=True)
 
-    if streamlit.button("Delete File"):
+    if st.button("Delete File"):
         utils.delete_file()
-        streamlit.success("File deleted successfully")
-        streamlit.dataframe(file_info_df, use_container_width=True)
+        st.success("File deleted successfully")
+        st.dataframe(file_info_df, use_container_width=True)
 
-    streamlit.subheader("Data Preview", divider=True)
+    st.subheader("Data Preview", divider=True)
     df = None
-    with streamlit.expander("Raw Data", expanded=True):
+    with st.expander("Raw Data", expanded=True):
         try:
             df = utils.load_data()
             df.head()
-            streamlit.dataframe(df, use_container_width=True)
+            st.dataframe(df, use_container_width=True)
         except Exception:
-            streamlit.warning("No data available. Please upload a CSV file.")
+            st.warning("No data available. Please upload a CSV file.")
 
-    with streamlit.expander("Plot Data", expanded=True):
+    with st.expander("Plot Data", expanded=True):
         if df is None or df.empty:
-            streamlit.warning("No data available. Please upload a CSV file.")
+            st.warning("No data available. Please upload a CSV file.")
         else:
-            x = list(df.columns)[0:]
-            y = list(df.columns)[0:]
-            graph_type = streamlit.sidebar.selectbox(
-                "Select your Graph Type:", ("Scatter", "Bar"), index=0
+            columns = list(df.columns)[0:]
+            x_choice = st.sidebar.selectbox("X-Axis:", columns, index=0)
+            y_choice = st.sidebar.selectbox(
+                "Y-Axis:", columns, index=len(columns) - 1
             )
-            x_choice = streamlit.sidebar.selectbox(
-                "Select your X-Axis:", x, index=0
+            color = st.sidebar.selectbox(
+                "Color:", columns, index=len(columns) - 1
             )
-            y_choice = streamlit.sidebar.selectbox(
-                "Select your Y-Axis:", y, index=len(y) - 1
+            fig = px.line(
+                df,
+                x=x_choice,
+                y=y_choice,
+                color=color,
+                markers=True,
             )
-            category = streamlit.sidebar.text_input(
-                "Enter Category", value=f"{y_choice} over {x_choice}"
-            )
-            graph_type_selected = None
-            if graph_type == "Scatter":
-                graph_type_selected = plotly.graph_objects.Scatter(
-                    x=df[x_choice],
-                    y=df[y_choice],
-                    name=category or "Share Prices (in USD)",
-                )
-            elif graph_type == "Bar":
-                graph_type_selected = plotly.graph_objects.Bar(
-                    x=df[x_choice],
-                    y=df[y_choice],
-                    name=category or "Share Prices (in USD)",
-                )
-            fig = plotly.graph_objects.Figure(graph_type_selected)
-            fig.update_layout(
-                title=dict(text="Apple Share Prices over time (2014)"),
-                plot_bgcolor="rgb(230, 230,230)",
-                showlegend=True,
-            )
-            streamlit.plotly_chart(fig, use_container_width=True)
+            st.plotly_chart(fig, use_container_width=True)
 
 
 if __name__ == "__main__":
